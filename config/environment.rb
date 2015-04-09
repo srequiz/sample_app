847f028bd1704010032d0d4ab8894d9abaa21a54
Jonathán Rodriguez <srequiz@gmail.com>
2015-04-09 11:11:14 +0000

    use SSL and puma server in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
