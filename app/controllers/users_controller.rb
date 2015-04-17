28befc5780ab8331c80256b561bea37456653d88
Jonathán Rodriguez <srequiz@gmail.com>
2015-04-17 13:11:52 +0000

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,9 +45,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      flash[:success] = "El usuario ha sido creado!"
-      log_in @user
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
       # Handle a successful save.
     else
       render 'new'
